ec8e9da2d0e21b972883cca21e9360ffc4cebdaa
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-10-02 14:37:20 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -26,10 +26,16 @@ terraform {
   }
 }
 
+# =========================
+# Azure provider
+# =========================
 provider "azurerm" {
   features {}
 }
 
+# =========================
+# Kubernetes provider (via AKS kubeconfig)
+# =========================
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
@@ -37,12 +43,14 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
 }
 
+# =========================
+# Helm provider (via AKS kubeconfig)
+# =========================
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
   }
 }
-
