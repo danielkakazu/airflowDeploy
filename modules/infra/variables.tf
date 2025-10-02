e05d6f494f3686d48c117ee7f53f44aa8742a981
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-10-02 15:08:50 -0300

    Update variables.tf

--- modules/infra/variables.tf
+++ modules/infra/variables.tf
@@ -1,5 +1,20 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "vnet_name" { type = string }
-variable "db_admin_user" { type = string }
-variable "db_admin_password" { type = string, sensitive = true }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "db_admin_user" {
+  type = string
+}
+
+variable "db_admin_password" {
+  type      = string
+  sensitive = true
+}
