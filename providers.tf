f7e1a59fd19ec8cd74328eb7f860851c1899fdc7
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-10-02 14:49:13 -0300

    Update providers5.tf

--- providers.tf
+++ providers.tf
@@ -31,12 +31,24 @@ provider "azurerm" {
   features {}
 }
 
-# Kubernetes provider (usaremos alias depois que AKS existir)
+# Kubernetes provider (AKS)
 provider "kubernetes" {
   alias = "aks"
+
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
 }
 
-# Helm provider (usaremos alias depois que AKS existir)
+# Helm provider (AKS)
 provider "helm" {
   alias = "aks"
+
+  kubernetes = {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  }
 }
