284f7fb3904cf4059911f14c1f1f435f590259db
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-10-02 14:38:55 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -26,31 +26,30 @@ terraform {
   }
 }
 
-# =========================
-# Azure provider
-# =========================
 provider "azurerm" {
   features {}
 }
 
-# =========================
-# Kubernetes provider (via AKS kubeconfig)
-# =========================
+# Kubernetes provider com alias, dependente do cluster AKS
 provider "kubernetes" {
+  alias                  = "aks"
   host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+
+  depends_on = [azurerm_kubernetes_cluster.aks]
 }
 
-# =========================
-# Helm provider (via AKS kubeconfig)
-# =========================
+# Helm provider com alias, dependente do cluster AKS
 provider "helm" {
+  alias      = "aks"
   kubernetes = {
     host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
   }
+
+  depends_on = [azurerm_kubernetes_cluster.aks]
 }
